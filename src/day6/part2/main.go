3bf02f48c39397937b015fce747875e42e2518a0
Laseruss <loden.micke@gmail.com>
2022-12-06 16:42:41 +0100

    solutions for day 6

---
+++ src/day6/part2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	text, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	chars := map[byte]int{}
+	res := 0
+
+	for i := 0; i < len(text); i++ {
+		if idx, ok := chars[text[i]]; ok {
+			i = idx
+			chars = map[byte]int{}
+			continue
+		}
+
+		chars[text[i]] = i
+
+		if len(chars) == 14 {
+			res = i + 1
+			break
+		}
+	}
+
+	fmt.Println(res)
+}
