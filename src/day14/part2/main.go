8bcf648490916b7c71bafacfa71a0b895c215ab9
Laseruss <loden.micke@gmail.com>
2022-12-20 18:48:12 +0100

    day 14 part 2, ugly code don't look

--- src/day14/part2/main.go
+++ src/day14/part2/main.go
@@ -12,6 +12,10 @@ type cord struct {
 	x, y int
 }
 
+type obj struct {
+	start, end int
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -20,6 +24,22 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	coordinates := parseInput(scanner)
+
+	/*
+		objects := map[int][]obj{}
+
+		for _, row := range coordinates {
+			for i := 0; i < len(row)-1; i++ {
+				if row[i].x != row[i+1].x {
+					if row[i].x < row[i+1].x {
+						objects[row[i].y] = append(objects[row[i].y], obj{row[i].x, row[i+1].x})
+					} else {
+						objects[row[i].y] = append(objects[row[i].y], obj{row[i+1].x, row[i].x})
+					}
+				}
+			}
+		}
+	*/
 
 	minX := coordinates[0][0].x
 	maxX := coordinates[0][0].x
@@ -39,9 +59,9 @@ func main() {
 		}
 	}
 
-	cave := make([][]string, maxY+1)
+	cave := make([][]string, maxY+2)
 	for i := range cave {
-		cave[i] = make([]string, maxX-minX+1)
+		cave[i] = make([]string, 1000)
 	}
 
 	for _, row := range coordinates {
@@ -49,21 +69,21 @@ func main() {
 			if row[i].x == row[i+1].x {
 				if row[i].y > row[i+1].y {
 					for j := row[i+1].y; j <= row[i].y; j++ {
-						cave[j][row[i].x-minX] = "#"
+						cave[j][row[i].x] = "#"
 					}
 				} else {
 					for j := row[i].y; j <= row[i+1].y; j++ {
-						cave[j][row[i].x-minX] = "#"
+						cave[j][row[i].x] = "#"
 					}
 				}
 			} else {
 				if row[i].x > row[i+1].x {
 					for j := row[i+1].x; j <= row[i].x; j++ {
-						cave[row[i].y][j-minX] = "#"
+						cave[row[i].y][j] = "#"
 					}
 				} else {
 					for j := row[i].x; j <= row[i+1].x; j++ {
-						cave[row[i].y][j-minX] = "#"
+						cave[row[i].y][j] = "#"
 					}
 
 				}
@@ -79,19 +99,21 @@ func main() {
 		}
 	}
 
-	// Drop sand
-	dropIdx := 500 - minX
+	dropIdx := 500
 	dropping := true
 	grains := 0
 
 	for {
-		grains++
 		y := 0
 		x := dropIdx
 		falling := true
 		for falling {
-			if x <= 0 || x == len(cave[0])-1 || y == len(cave)-1 {
+			if cave[0][500] == "o" {
 				dropping = false
+				break
+			}
+			if y == len(cave)-1 {
+				cave[y][x] = "o"
 				break
 			}
 			// check if it can fall left and then right
@@ -111,12 +133,55 @@ func main() {
 		if !dropping {
 			break
 		}
+		grains++
 	}
 
-	for _, row := range cave {
-		fmt.Println(row)
+	fmt.Println(grains)
+
+	/*
+		sumObs := 0
+
+		for _, row := range cave {
+			fmt.Println(strings.Join(row, " "))
+			for _, v := range row {
+				if v == "#" {
+					sumObs++
+				}
+			}
+		}
+
+		sumBlocked := 0
+		for depth, blockers := range objects {
+			d := depth
+			for _, block := range blockers {
+				w := block.end - block.start + 1
+				for d < maxY+1 && w > 2 {
+					d++
+					c := true
+					for _, v := range objects[d] {
+						if (v.start >= block.start && v.start <= block.end) || (v.end >= block.start && v.end <= block.end) {
+							c = false
+						}
+					}
+					if !c {
+						break
+					}
+					w = w - 2
+					sumBlocked += w
+				}
+			}
+		}
+	*/
+
+}
+
+func totalGrains(y int) int {
+	res := 0
+	for i := 1; i <= y+2; i++ {
+		res += i*2 - 1
 	}
-	fmt.Println(grains - 1)
+
+	return res
 }
 
 func parseInput(scanner *bufio.Scanner) [][]cord {
